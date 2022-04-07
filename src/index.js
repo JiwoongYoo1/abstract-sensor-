ab69dd56d3fb313db93f8edb6dd39a29274d15c1
JiwoongYoo1 <dbwldnd89@gmail.com>
2022-04-07 11:51:57 +0900

    테스트코드 매운맛

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
-class Sensor {}
+class Sensor {
+    constructor(name){
+        this.name = name     
+        this.powerStatus = 'off'  
+        this.reportingInterval = 10000 
 
-class IotServer {}
+        
+     }
+ 
+  
+      turn(onoff){    
+         this.onoff = onoff    
+         if(this.powerStatus === 'on' && this.onoff === 'on'){
+             throw new Error()
+         }else if(this.powerStatus === 'on' && this.onoff === 'off'){
+            this.powerStatus = 'off'
+         }else{
+            this.powerStatus = onoff
+         }
+        
+         this.status = 'idle'
+        
+         
+         setTimeout(() => {
+             this.status = 'sensingDistance';
+         setTimeout(() => {
+             this.status = 'reportingData' 
+         setTimeout(() => {
+             this.status = 'idle'           
+           }, 1000);
+          }, 500);
+         }, this.reportingInterval);
+       
+         
+     }
+     
+      
+}   
+    
+class IotServer {
+    start(sensor){
+        this.sensor = sensor
+    }
+    publish(server){
+        const{deviceId, actionId, payload} =server
+        this.deviceId = deviceId,
+        this.actionId = actionId,
+        this.payload = payload
+
+        if(this.actionId === 'CHANGE_REPORTING_INTERVAL' && this.sensor[0].powerStatus === 'on'){
+            this.sensor[0].reportingInterval = this.payload
+        }
+    }
+}
 
 module.exports = {
     Sensor,
